56c552b44cfc4c87df468d1b8acb796c80f365e6
Carlos PC <xdeus.v2@gmail.com>
2024-02-16 14:50:15 -0600

    Added a UI for the auth key login

--- PasswordManager/login.py
+++ PasswordManager/login.py
@@ -2,23 +2,29 @@ import os
 from dotenv import load_dotenv
 from cryptography.fernet import Fernet
 from manage_dotenv import write_dotenv_encryption_key
+
+from tkinter import *
+from tkinter import messagebox
 load_dotenv()
 
-dir = "Python 100 DAYS/PasswordManager/"
+os.chdir("Python 100 DAYS/PasswordManager")
+print(os.getcwd())
 
 try:
-    key = os.getenv("ENCRYPTION_KEY")
+    
+    key = os.getenv("ENCRYPTION_KE")
     encryptor = Fernet(key)
 except TypeError as err:
-    print("\nNo encryption keys found.\n")
-    generate_encryption_key = input ("Would you like to generate your encryption key? (y/n): ")
-    if (generate_encryption_key == 'y'):
+    info = messagebox.showinfo(title="ENCRYPTION KEY",message="Your encryption key was NOT succesfully loaded.")
+    generate_encryption_key = messagebox.askquestion(title="GENERATE ENCRYPTION KEY",message="Would you like to generate your encryption key?")
+    if (generate_encryption_key == 'yes'):
         key = Fernet.generate_key()
-        write_dotenv_encryption_key(dir,key="ENCRYPTION_KEY",value=key.decode())
+        write_dotenv_encryption_key(os.getcwd(),key="ENCRYPTION_KEY",value=key.decode())
         print(f"Your encryption key is: {key.decode()}")
         encryptor = Fernet(key)
     else:
         print(f"\nWill not proceed if there is not an encryption (key) for encryptor: {err}")
 else:
+    info = messagebox.showinfo(title="ENCRYPTION KEY",message="Your encryption key was succesfully loaded")
     encryptor = Fernet(key)
     
