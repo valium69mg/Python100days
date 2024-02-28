0ec9e709bfa61b979283fe277ecd52eaa6bd0f98
Carlos PC <xdeus.v2@gmail.com>
2024-02-28 14:47:09 -0600

    Added commentary for further explaation

--- flight_deal_finder/main.py
+++ flight_deal_finder/main.py
@@ -63,7 +63,9 @@ def lowest_price_flight(json=json):
             if (dict["price"] == lowest_price):
                 return dict
 
-
+"""
+SAVE FLIGHT TO SPREAD SHEET FUNCTION
+"""
 
 def save_flight_to_spreadsheet(flight):
     body = {
@@ -72,6 +74,10 @@ def save_flight_to_spreadsheet(flight):
     res = requests.post(url=SHEET_URL,json=body)
     res.raise_for_status()
 
+"""
+COMPARE IF THE FLIGHT IS CHEAPER THAN THE ALREADY
+SAVED FLIGHT ON THE SHEETS
+"""
 
 def is_flight_cheaper(flight_to_compare):
     res = requests.get(url=SHEET_URL)
@@ -81,8 +87,18 @@ def is_flight_cheaper(flight_to_compare):
         return True
     else:
         return False
+    
+"""
+COMPUTE THE CHEAPEST FLIGHT OF TODAY
+"""
 
 flight_to_save = lowest_price_flight()
+
+"""
+MAIN LOGIC
+IF THE FLIGHT FOUND TODAY IS CHEAPER THAN THE SPREED SHEET FLIGHT
+IT ADDS IT TO THE SPREAD SHEET
+"""
 
 if (is_flight_cheaper(flight_to_compare=flight_to_save) == True):
     save_flight_to_spreadsheet(flight_to_save)
