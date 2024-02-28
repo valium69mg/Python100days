def723efb8db532647ff13704bd128a3ff033749
Carlos PC <xdeus.v2@gmail.com>
2024-02-28 15:26:23 -0600

    Debugging code

--- flight_deal_finder/main.py
+++ flight_deal_finder/main.py
@@ -7,7 +7,8 @@ load_dotenv()
 
 SHEET_URL = "https://api.sheety.co/fbfbd5d717d9ebe861b7db1757ba3cfe/flightDealFinder/hoja1"
 TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
-
+DATE_FROM="01/06/2024"
+DATE_TO="08/06/2024"
 
 """
 TEQUILA KIWI API REQUESTS
@@ -19,8 +20,8 @@ header = {
 json = {
     "fly_from":"airport:QRO",
     "fly_to":"CUN",
-    "date_from":"01/06/2024",
-    "date_to":"08/06/2024"
+    "date_from":DATE_FROM,
+    "date_to":DATE_TO
 }
 
 def lowest_price_flight(json=json):
@@ -82,12 +83,40 @@ SAVED FLIGHT ON THE SHEETS
 def is_flight_cheaper(flight_to_compare):
     res = requests.get(url=SHEET_URL)
     res.raise_for_status()
-    price = res.json()['hoja1'][0]["price"]
-    if (price > flight_to_compare["price"]):
-        return True
-    else:
+    # IF THE IS NO FLIGHT REGISTERED WE SAVE IT 
+    if (res.json()['hoja1'] == []):
+        save_flight_to_spreadsheet(flight_to_compare)
         return False
-    
+    else:
+        # COMPARE FLIGHTS
+        price = res.json()['hoja1'][0]["price"]
+        if (int(price) > int(flight_to_compare["price"])):
+            return True
+        else:
+            return False
+
+"""
+FUNCTION TO FIND THE ID OF THE CURRENT SAVED FLIGHT
+"""
+
+def find_flight_id():
+    res = requests.get(url=SHEET_URL)
+    res.raise_for_status()
+    id = res.json()["hoja1"][0]["id"]
+    return id
+
+"""
+IF A FLIGHT IS ALREADY SAVED WE NEED TO UPDATE THE FLIGHT INFO
+AND WE DO IT WITH A PUT REQUEST
+"""
+        
+def update_flight(new_flight,id=find_flight_id()):
+    body = {
+        "hoja1":new_flight
+    }
+    res = requests.put(url=SHEET_URL+f"/{id}",json=body)
+    res.raise_for_status()
+
 """
 COMPUTE THE CHEAPEST FLIGHT OF TODAY
 """
@@ -100,8 +129,21 @@ IF THE FLIGHT FOUND TODAY IS CHEAPER THAN THE SPREED SHEET FLIGHT
 IT ADDS IT TO THE SPREAD SHEET
 """
 
+test_flight = {
+    "hoja1":{
+        "from":"QRO",
+        "to":"CUN",
+        "flight":"69",
+        "price":"69",
+        "localDate":"Hello",
+        "arrivalDate":"World"
+    }
+}
+    
+
 if (is_flight_cheaper(flight_to_compare=flight_to_save) == True):
-    save_flight_to_spreadsheet(flight_to_save)
+    update_flight(new_flight=flight_to_save)
+    print("Flight info updated!")
 else:
     print("There are no flights cheaper")
 
